87efe060331ce4b6a4a32825d3d9ac6cc662f377
Nils Wentzell <nils.wentzell@gmail.com>
2017-12-18 13:50:20 +0100

    Comparison of equal-time correlation function chi3

--- common/plot.py
+++ common/plot.py
@@ -7,36 +7,47 @@ from h5 import HDFArchive
 from triqs.plot.mpl_interface import oplot, plt
 from glob import glob
 from os.path import basename
+import numpy as np
 
-# === Load Green function for every solver and calculate self-energy
+# === Load chi3pp and chi3ph for every solver
 
 solver_lst = [ basename(f).strip('.h5') for f in glob('results/*.h5') ]
-marker_lst = ['-x', '-+', '-^', '-v', '-<', '->', '-*', '-p']
-G, Sigma = {}, {}
+chi3pp_iw, chi3ph_iw = {}, {}
 
 for solver in solver_lst:
     dat = HDFArchive('results/' + solver + '.h5','r')
-    G[solver] = dat['G']
-    Sigma[solver] = G0_iw.copy()
-    Sigma[solver] << inverse(G0_iw) - inverse(G[solver])
+    chi3pp_iw[solver] = dat['chi3pp_iw']
+    chi3ph_iw[solver] = dat['chi3ph_iw']
+    
+# === For every block and solver, plot chi3pp and chi3ph
 
-# === For every block and solver, plot Green function and Self energy
-
-block_lst = list(G[solver_lst[0]].indices)
+block_lst = [bl for bl in chi3pp_iw[solver_lst[0]].indices]
 n_blocks = len(block_lst)
+n_solvers = len(solver_lst)
 
-for g, name in [[G, 'G'], [Sigma, '$\Sigma$']]:
+for chi3, name in [[chi3pp_iw, r'$\chi_3^{\rm PP}$'], [chi3ph_iw, r'$\chi_3^{\rm PH}$']]:
 
-    plt.subplots(n_blocks,1,figsize=(10,6*n_blocks))
-
-    for i, block in enumerate(block_lst,1):
-        fig = plt.subplot(n_blocks,1,i)
-        fig.set_title(name + "[" + block + "]")
+    for block in block_lst:
+        
+        vmin = min([np.min(chi3[solver][block].data.real) for solver in solver_lst])
+        vmax = max([np.max(chi3[solver][block].data.real) for solver in solver_lst])
+        
+        if vmin == vmax: continue
+            
+        plt.subplots(n_solvers, figsize=(3*n_solvers + 1,3))
+        plt.suptitle("Re " + name + "[" + str(block) + r"][0,0,0,0]", fontsize=14, y=1.1)
+        count = 1
+        
         for solver in solver_lst:
-            marker = marker_lst[solver_lst.index(solver)]
-            oplot(g[solver][block][0,0], marker, name = name + "[0,0]_%s" % solver)
-        plt.xlabel("$\omega_n$")
-        plt.ylabel(name + "[" + block + "]$(i\omega_n)$")
+            fig = plt.subplot(1, n_solvers, count); 
+            plt.pcolormesh(chi3[solver][block].data[:,:,0,0,0,0].real, vmin=vmin, vmax=vmax)
+            fig.set_title(solver)
+            fig.axes.set_aspect('equal')
+            plt.xlabel(r"$\Omega$")
+            plt.ylabel(r"$\omega$")
+            count += 1
+            
+        plt.colorbar() 
+        plt.tight_layout()
 
-    plt.tight_layout()
     plt.show()
