e73348219dc15b776426177f8c621c64d51b6f70
Nils Wentzell <nils.wentzell@gmail.com>
2018-04-06 21:28:09 +0200

    Adding Trimer model with notebook and scripts

---
+++ Trimer/model.py
@@ -0,0 +1,82 @@
+import sys, os
+sys.path.append(os.getcwd() + '/../common')
+from util import *
+
+from pytriqs.gf import Gf, MeshImFreq, iOmega_n, inverse
+from pytriqs.operators import c, c_dag, n
+from pytriqs.operators.util.hamiltonians import h_int_kanamori
+from itertools import product
+from numpy import matrix, array, diag
+
+# ==== System Parameters ====
+beta = 5.                           # Inverse temperature
+mu = 0.25                           # Chemical potential
+eps = array([0.0, 0.1, 0.2])        # Impurity site energies
+t = 1.                              # Hopping between impurity sites
+
+eps_bath = array([0.2, 0.15, 0.1])  # Bath site energies
+t_bath = 0.1                        # Hopping between bath sites
+
+U = 1.                              # Density-density interaction for opposite spins
+Up = 0.3                            # Density-density interaction for equal spins
+J = 0.5                             # Hunds coupling
+
+spin_names = ['up', 'dn']
+orb_names  = [0, 1, 2]
+
+# Non-interacting impurity hamiltonian in matrix representation
+h_0_mat = diag(eps - mu) - matrix([[0, t, 0],
+                                   [t, 0, t],
+                                   [0, t, 0]])
+
+# Bath hamiltonian in matrix representation
+h_bath_mat = diag(eps_bath) - matrix([[0,       t_bath, 0       ],
+                                      [t_bath,  0,      t_bath  ],
+                                      [0,       t_bath, 0       ]])
+
+# Coupling matrix
+V_mat = matrix([[1., 0., 0.],
+                [0., 1., 0.],
+                [0., 0., 1.]])
+
+# ==== Local Hamiltonian ====
+c_dag_vec = { s: matrix([[c_dag(s,o) for o in orb_names]]) for s in spin_names }
+c_vec =     { s: matrix([[c(s,o)] for o in orb_names]) for s in spin_names }
+
+h_0 = sum(c_dag_vec[s] * h_0_mat * c_vec[s] for s in spin_names)[0,0]
+
+h_int = h_int_kanamori(spin_names, orb_names,
+                        array([[0,      Up-3*J, 0       ],
+                               [Up-3*J, 0,      Up-3*J  ],
+                               [0,      Up-3*J, 0       ]]),    # Interaction for equal spins
+                        array([[U,      U-2*J,  0       ],
+                               [U-2*J,  U,      U-2*J   ],
+                               [0,      U-2*J,  0       ]]),    # Interaction for opposite spins
+                        J,True)
+
+h_loc = h_0 + h_int
+
+# ==== Bath & Coupling hamiltonian ====
+orb_bath_names = ['b_' + str(o) for o in orb_names]
+c_dag_bath_vec = { s: matrix([[c_dag(s, o) for o in orb_bath_names]]) for s in spin_names }
+c_bath_vec =     { s: matrix([[c(s, o)] for o in orb_bath_names]) for s in spin_names }
+
+h_bath = sum(c_dag_bath_vec[s] * h_bath_mat * c_bath_vec[s] for s in spin_names)[0,0]
+h_coup = sum(c_dag_vec[s] * V_mat * c_bath_vec[s] + c_dag_bath_vec[s] * V_mat * c_vec[s] for s in spin_names)[0,0] # FIXME Adjoint
+
+# ==== Total impurity hamiltonian ====
+h_imp = h_loc + h_coup + h_bath
+
+# ==== Green function structure ====
+gf_struct = [ [s, orb_names] for s in spin_names ]
+
+# ==== Hybridization Function ====
+n_iw = 10
+iw_mesh = MeshImFreq(beta, 'Fermion', n_iw)
+Delta = BlockGf_from_struct(mesh=iw_mesh, struct=gf_struct)
+Delta << inverse(iOmega_n - V_mat * h_bath_mat * V_mat);
+
+# ==== Non-Interacting Impurity Green function  ====
+G0_iw = Delta.copy()
+G0_iw['up'] << inverse(iOmega_n - h_0_mat - Delta['up']) # FIXME Should work for BlockGf
+G0_iw['dn'] << inverse(iOmega_n - h_0_mat - Delta['dn'])
