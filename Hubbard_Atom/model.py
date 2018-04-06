734e7963ef51bebd24b3fd81be6fee1e12619946
Nils Wentzell <nils.wentzell@gmail.com>
2018-04-06 21:01:20 +0200

    Minor update Hubbard Atom model.py

--- Hubbard_Atom/model.py
+++ Hubbard_Atom/model.py
@@ -12,6 +12,9 @@ mu = 2.             # Chemical potential
 U = 5.              # On-site density-density interaction
 h = 0.2             # Local magnetic field
 
+spin_names = ['up', 'dn']
+orb_names  = [0]
+
 # ==== Local Hamiltonian ====
 h_0 = - mu*( n('up',0) + n('dn',0) ) - h*( n('up',0) - n('dn',0) )
 h_int = U * n('up',0) * n('dn',0)
@@ -22,16 +25,15 @@ h_bath, h_coup = 0, 0
 
 # ==== Total impurity hamiltonian and fundamental operators ====
 h_imp = h_loc + h_coup + h_bath
-fundamental_operators = [ c('dn',0), c('up',0) ] # FIXME To be extracted
 
 # ==== Green function structure ====
-gf_struct = [ ['up',[0]], ['dn',[0]] ]
+gf_struct = [ [s, orb_names] for s in spin_names ]
 
 # ==== Hybridization Function ====
 n_iw = 20
 iw_mesh = MeshImFreq(beta, 'Fermion', n_iw)
 Delta = BlockGf_from_struct(mesh=iw_mesh, struct=gf_struct)
-Delta << 0.0; 
+Delta << 0.0;
 
 # ==== Non-Interacting Impurity Green function  ====
 G0_iw = BlockGf_from_struct(mesh=iw_mesh, struct=gf_struct)
