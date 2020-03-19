b4317825053aa452be7a34fa0afd0577623ec358
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2020-03-19 16:01:54 +0900

    Added alps_cthyb to HubbardAtom

--- common/util.py
+++ common/util.py
@@ -3,7 +3,6 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from pytriqs.gf import *
 from pytriqs.operators import c, c_dag, n, dagger
-from pytriqs.utility import mpi
 
 # Get a list of all annihilation operators from a many-body operators
 def get_fundamental_operators(op):
@@ -13,8 +12,3 @@ def get_fundamental_operators(op):
             if not idx_lst.count([bl, orb]):
                 idx_lst.append([bl,orb])
     return [c(bl, orb) for bl, orb in idx_lst]
-
-# print on master node
-def mpi_print(arg):
-    if mpi.is_master_node():
-        print arg
---
+++ common/util_mpi.py
@@ -0,0 +1,9 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from pytriqs.utility import mpi
+
+# print on master node
+def mpi_print(arg):
+    if mpi.is_master_node():
+        print arg
