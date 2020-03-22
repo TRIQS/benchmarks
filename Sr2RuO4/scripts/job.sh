78895bb2a518dcbe8b56dfb70981ce1c7c00e63b
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2020-03-22 11:03:12 +0900

    Added results of ALPS/CT-HYB for Sr2RuO4

---
+++ Sr2RuO4/scripts/job.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#BSUB -n 24
+#BSUB -o test
+#BSUB -J test
+
+export DCORE_MPIRUN_COMMAND="/usr/share/lava/1.0/linux2.6-glibc2.12-x86_64/bin/intelmpi-mpirun -np 24"
+export DCORE_ALPSCTHYB_TIMELIMIT=600
+
+./alps_cthyb > output-alpscthyb
