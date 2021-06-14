94858e74f12ec0a4fa9264a19648492cf3ba8461
Nils Wentzell <nwentzell@flatironinstitute.org>
2021-06-14 09:56:01 -0400

    Add Dimer Model calculating G_tau instead of G_iw

---
+++ Dimer_GTAU/model.py
@@ -0,0 +1,80 @@
+import sys, os
+sys.path.append(os.getcwd() + '/../common')
+from util import *
+
+from triqs.gf import Gf, MeshImFreq, iOmega_n, inverse
+from triqs.operators import c, c_dag, n
+from triqs.operators.util import h_int_kanamori, U_matrix_kanamori
+from itertools import product
+from numpy import matrix, array, block, diag, eye
+from numpy.linalg import inv
+
+# ==== System Parameters ====
+beta = 5.                       # Inverse temperature
+mu = 0.0                        # Chemical potential
+eps = array([0.0, 0.1])         # Impurity site energies
+t = 0.2                         # Hopping between impurity sites
+
+eps_bath = array([0.27, -0.4])  # Bath site energies
+t_bath = 0.0                    # Hopping between bath sites
+
+U = 1.                          # Density-density interaction
+J = 0.2                         # Hunds coupling
+
+spin_names = ['up', 'dn']
+orb_names  = [0, 1]
+n_orb = len(orb_names)
+
+# Non-interacting impurity hamiltonian in matrix representation
+h_0_mat = diag(eps - mu) - matrix([[0, t],
+                                   [t, 0]])
+
+# Bath hamiltonian in matrix representation
+h_bath_mat = diag(eps_bath) - matrix([[0, t_bath],
+                                      [t_bath, 0]])
+
+# Coupling matrix
+V_mat = matrix([[1., 1.],
+                [1., 1.]])
+
+# ==== Local Hamiltonian ====
+c_dag_vec = { s: matrix([[c_dag(s,o) for o in orb_names]]) for s in spin_names }
+c_vec =     { s: matrix([[c(s,o)] for o in orb_names]) for s in spin_names }
+
+h_0 = sum(c_dag_vec[s] * h_0_mat * c_vec[s] for s in spin_names)[0,0]
+
+Umat, Upmat = U_matrix_kanamori(n_orb, U_int=U, J_hund=J)
+h_int = h_int_kanamori(spin_names, orb_names, Umat, Upmat, J, off_diag=True)
+
+h_imp = h_0 + h_int
+
+# ==== Bath & Coupling hamiltonian ====
+n_orb_bath = len(eps_bath)
+orb_bath_names = [o for o in range(n_orb, n_orb + n_orb_bath)]
+c_dag_bath_vec = { s: matrix([[c_dag(s, o) for o in orb_bath_names]]) for s in spin_names }
+c_bath_vec =     { s: matrix([[c(s, o)] for o in orb_bath_names]) for s in spin_names }
+
+h_bath = sum(c_dag_bath_vec[s] * h_bath_mat * c_bath_vec[s] for s in spin_names)[0,0]
+h_coup = sum(c_dag_vec[s] * V_mat * c_bath_vec[s] + c_dag_bath_vec[s] * V_mat * c_vec[s] for s in spin_names)[0,0] # FIXME Adjoint
+
+# ==== Total impurity hamiltonian ====
+h_tot = h_imp + h_coup + h_bath
+
+# ==== Green function structure ====
+gf_struct = [ [s, n_orb] for s in spin_names ]
+
+# ==== Non-Interacting Impurity Green function  ====
+n_iw = int(10 * beta)
+n_tau = 1001 # 2 * n_iw + 1
+iw_mesh = MeshImFreq(beta, 'Fermion', n_iw)
+tau_mesh = MeshImTime(beta, 'Fermion', n_tau)
+G0_iw = BlockGf(mesh=iw_mesh, gf_struct=gf_struct)
+h_tot_mat = block([[h_0_mat, V_mat     ],
+                   [V_mat.H, h_bath_mat]])
+for bl, iw in product(spin_names, iw_mesh):
+    G0_iw[bl][iw] = inv(iw.value * eye(2*n_orb) - h_tot_mat)[:n_orb, :n_orb]
+
+# ==== Hybridization Function ====
+Delta = G0_iw.copy()
+Delta['up'] << iOmega_n - h_0_mat - inverse(G0_iw['up'])
+Delta['dn'] << iOmega_n - h_0_mat - inverse(G0_iw['dn'])
