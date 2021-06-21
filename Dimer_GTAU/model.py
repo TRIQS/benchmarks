73cfdccfc7af000771372a70a3118ca525bbf210
Nils Wentzell <nwentzell@flatironinstitute.org>
2021-06-21 12:44:01 -0400

    Add various ways of calculating G_iw, add G_tau from legendre, update data and notebook files

--- Dimer_GTAU/model.py
+++ Dimer_GTAU/model.py
@@ -21,9 +21,9 @@ t_bath = 0.0                    # Hopping between bath sites
 U = 1.                          # Density-density interaction
 J = 0.2                         # Hunds coupling
 
-spin_names = ['up', 'dn']
-orb_names  = [0, 1]
-n_orb = len(orb_names)
+block_names = ['up', 'dn']
+n_orb = len(eps)
+n_orb_bath = len(eps_bath)
 
 # Non-interacting impurity Hamiltonian in matrix representation
 h_0_mat = diag(eps - mu) - matrix([[0, t],
@@ -38,30 +38,28 @@ V_mat = matrix([[1., 0.],
                 [0., 1.]])
 
 # ==== Local Hamiltonian ====
-c_dag_vec = { s: matrix([[c_dag(s,o) for o in orb_names]]) for s in spin_names }
-c_vec =     { s: matrix([[c(s,o)] for o in orb_names]) for s in spin_names }
+c_dag_vec = { s: matrix([[c_dag(s,o) for o in range(n_orb)]]) for s in block_names }
+c_vec =     { s: matrix([[c(s,o)] for o in range(n_orb)]) for s in block_names }
 
-h_0 = sum(c_dag_vec[s] * h_0_mat * c_vec[s] for s in spin_names)[0,0]
+h_0 = sum(c_dag_vec[s] * h_0_mat * c_vec[s] for s in block_names)[0,0]
 
 Umat, Upmat = U_matrix_kanamori(n_orb, U_int=U, J_hund=J)
-h_int = h_int_kanamori(spin_names, orb_names, Umat, Upmat, J, off_diag=True)
+h_int = h_int_kanamori(block_names, range(n_orb), Umat, Upmat, J, off_diag=True)
 
 h_imp = h_0 + h_int
 
 # ==== Bath & Coupling Hamiltonian ====
-n_orb_bath = len(eps_bath)
-orb_bath_names = [o for o in range(n_orb, n_orb + n_orb_bath)]
-c_dag_bath_vec = { s: matrix([[c_dag(s, o) for o in orb_bath_names]]) for s in spin_names }
-c_bath_vec =     { s: matrix([[c(s, o)] for o in orb_bath_names]) for s in spin_names }
+c_dag_bath_vec = { s: matrix([[c_dag(s, o) for o in range(n_orb, n_orb + n_orb_bath)]]) for s in block_names }
+c_bath_vec =     { s: matrix([[c(s, o)] for o in range(n_orb, n_orb + n_orb_bath)]) for s in block_names }
 
-h_bath = sum(c_dag_bath_vec[s] * h_bath_mat * c_bath_vec[s] for s in spin_names)[0,0]
-h_coup = sum(c_dag_vec[s] * V_mat * c_bath_vec[s] + c_dag_bath_vec[s] * V_mat.H * c_vec[s] for s in spin_names)[0,0]
+h_bath = sum(c_dag_bath_vec[s] * h_bath_mat * c_bath_vec[s] for s in block_names)[0,0]
+h_coup = sum(c_dag_vec[s] * V_mat * c_bath_vec[s] + c_dag_bath_vec[s] * V_mat.H * c_vec[s] for s in block_names)[0,0]
 
 # ==== Total impurity Hamiltonian ====
 h_tot = h_imp + h_coup + h_bath
 
 # ==== Green function structure ====
-gf_struct = [ [s, n_orb] for s in spin_names ]
+gf_struct = [ (s, n_orb) for s in block_names ]
 
 # ==== Hybridization Function ====
 n_iw = int(10 * beta)
--- common/util.py
+++ common/util.py
@@ -2,6 +2,7 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from triqs.gf import *
+from triqs.gf.tools import fit_legendre
 from triqs.operators import c, c_dag, n, dagger
 
 # Get a list of all annihilation operators from a many-body operators
@@ -12,3 +13,16 @@ def get_fundamental_operators(op):
             if not idx_lst.count([bl, orb]):
                 idx_lst.append([bl,orb])
     return [c(bl, orb) for bl, orb in idx_lst]
+
+def fit_G_l(Gt):
+    G_l = fit_legendre(Gt, 5)
+
+    for n_l in range(7, 41, 2):
+        G_l_new = fit_legendre(Gt, n_l)
+        if abs(G_l_new['up'][0,0].data[n_l-3]) < abs(G_l_new['up'][0,0].data[n_l-1]):
+            print("Optimal number of legendre coefficients " + str(n_l - 2))
+            return G_l
+        else:
+            G_l = G_l_new
+
+    return G_l
